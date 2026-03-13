d9fc60c703e838d40e98f4fec62f8280efe8b6b5
Pentillion <maurosterpin2@gmail.com>
2026-03-13 16:06:12 +0100

    Update ping_tool to ping

--- src/bin/ping.rs
+++ src/bin/ping.rs
@@ -7,7 +7,7 @@ use rust_network_tools::{ICMP_ECHO_REPLY, ICMP_ECHO_REQUEST, ICMP_ID, calculate_
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        eprintln!("Usage: ping_tool <target-ip>");
+        eprintln!("Usage: ping <target-ip>");
         return Ok(());
     }
 
